a721f6729621d6979f50574eceb73d5179b7448a
keafea <keafea@hotmail.com>
2016-03-10 21:00:22 +0800

    commented one line

--- testapp/src/testapp/Hello.java
+++ testapp/src/testapp/Hello.java
@@ -3,6 +3,6 @@ package testapp;
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello World");
-		System.out.println("Hi Everyone");
+		//System.out.println("Hi Everyone");
 	}
 }
